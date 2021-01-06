703e745a36f184cd79fb8e992a60c54f93c231c5
Bergur Hallgrímsson <bergur@bergur.is>
2021-01-06 07:11:50 +0000

    linting

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div>
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Stuðningsbankinn</a>
+      <a
+        class="navbar-brand col-md-3 col-lg-2 me-0 px-3"
+        href="#"
+      >
+        Stuðningsbankinn
+      </a>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
+          <a
+            class="nav-link"
+            href="#"
+          >
+            Skrá út
+          </a>
         </li>
       </ul>
     </header>
 
     <div class="container-fluid">
       <div class="row">
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <nav
+          id="sidebarMenu"
+          class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+        >
           <div class="position-sticky pt-3">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  <span data-feather="home"></span>
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href="#"
+                >
+                  <span data-feather="home" />
                   Staðir
                 </a>
               </li>
