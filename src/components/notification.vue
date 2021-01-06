d326eb6cef4448ff6c8398285427d8ebd55a0c66
Bergur Hallgrímsson <bergur@bergur.is>
2021-01-06 22:29:07 +0000

    add notification component

---
+++ src/components/notification.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    v-if="show"
+    class="notification"
+    :class="classes"
+  >
+    <button
+      class="delete"
+      @click="close()"
+    />
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Notification',
+  props: {
+    message: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    success: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    error: {
+      type: Boolean,
+      required: false,
+      default: false
+
+    }
+  },
+  data () {
+    return {
+      successNotification: this.success,
+      errorNotification: this.error
+    }
+  },
+  computed: {
+    classes () {
+      return {
+        'is-success': this.success,
+        'is-danger': this.error
+      }
+    },
+    show () {
+      return this.successNotification || this.errorNotification
+    }
+  },
+  watch: {
+    error (val) {
+      this.errorNotification = val
+    },
+    success (val) {
+      this.successNotification = val
+    }
+  },
+  methods: {
+    close () {
+      this.successNotification = false
+      this.errorNotification = false
+    }
+  }
+}
+</script>
