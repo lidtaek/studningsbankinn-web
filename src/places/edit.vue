e8096e39402deb2b968d5d9389222af7daafe6a5
Bergur Hallgrímsson <bergur@bergur.is>
2021-01-06 22:29:25 +0000

    Add inputs for every field

--- src/places/edit.vue
+++ src/places/edit.vue
@@ -9,33 +9,107 @@
         class="card-content"
         @submit.prevent
       >
-        <div class="field">
-          <label class="label">Nafn</label>
-          <div class="control">
-            <input
-              v-model="place.name"
-              class="input"
-              type="text"
-            >
+        <Notification
+          :message="message"
+          :success="success"
+          :error="error"
+        />
+
+        <div class="columns">
+          <div class="field column is-12">
+            <label class="label">Nafn:</label>
+            <div class="control">
+              <input
+                v-model="place.name"
+                class="input"
+                type="text"
+              >
+            </div>
           </div>
         </div>
 
-        <div class="field is-grouped">
-          <div class="control">
-            <button
-              class="button is-primary"
-              @click="save()"
-            >
-              Vista
-            </button>
+        <div class="columns">
+          <div class="field column is-12">
+            <label class="label">Lýsing:</label>
+            <div class="control">
+              <textarea
+                v-model="place.description"
+                class="textarea"
+                placeholder="Textarea"
+              />
+            </div>
           </div>
-          <div class="control">
-            <button
-              class="button is-danger is-light"
-              @click="del()"
-            >
-              Eyða
-            </button>
+        </div>
+
+        <div class="columns">
+          <div class="field column is-6">
+            <label class="label">Vefsíða:</label>
+            <div class="control">
+              <input
+                v-model="place.website"
+                class="input"
+                type="text"
+              >
+            </div>
+          </div>
+
+          <div class="field column is-6">
+            <label class="label">Sími:</label>
+            <div class="control">
+              <input
+                v-model="place.phone"
+                class="input"
+                type="text"
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="columns">
+          <div class="field column is-6">
+            <label class="label">Heimilisfang:</label>
+            <div class="control">
+              <input
+                v-model="place.address"
+                class="input"
+                type="text"
+              >
+            </div>
+          </div>
+
+          <div class="field column is-6">
+            <label class="label">Póstnúmer:</label>
+            <div class="control">
+              <input
+                v-model="place.postcode"
+                class="input"
+                type="text"
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="columns">
+          <div class="field column is-6">
+            <div class="control">
+              <button
+                class="button is-primary"
+                @click="save()"
+              >
+                Vista
+              </button>
+            </div>
+          </div>
+
+          <div class="field column is-6 has-text-right">
+            <div class="control">
+              <button
+                class="button is-danger is-light"
+                @click="del()"
+              >
+                Eyða
+              </button>
+            </div>
           </div>
         </div>
       </form>
@@ -46,14 +120,19 @@
 <script>
 import agent from 'superagent'
 import Hero from '../components/hero'
+import Notification from '../components/notification'
 export default {
   name: 'PlacesList',
   components: {
-    Hero
+    Hero,
+    Notification
   },
   data () {
     return {
-      place: {}
+      place: {},
+      message: '',
+      success: false,
+      error: false
     }
   },
   computed: {
@@ -67,31 +146,57 @@ export default {
     const id = this.$route.params.id
 
     if (!isNaN(Number(id))) {
-      agent.get(apiUrl + '/?id=' + id).then(res => {
-        console.log(res.body)
-        if (res.body.length === 1) {
-          this.place = res.body[0]
-        }
-      })
+      agent
+        .get(apiUrl + '/?id=' + id)
+        .then(res => {
+          if (res.body.length === 1) {
+            this.place = res.body[0]
+          } else {
+            this.error = true
+            this.message = 'Staður fannst ekki'
+          }
+        })
+        .catch(e => {
+          this.error = true
+          this.message = 'Staður fannst ekki'
+        })
     }
   },
   methods: {
     save () {
+      this.success = false
       const apiUrl = process.env.STUDNINGSBANKINN_API_URL + '/places'
       const method = this.place.id ? 'put' : 'post'
+
       agent(method, apiUrl)
         .send(this.place)
         .then(res => {
-          console.log(res.body)
+          if (res.body.id > 0) {
+            this.success = true
+            this.message = 'Aðgerð tókst'
+          }
+        })
+        .catch(e => {
+          this.error = true
+          this.message = 'Ekki tókst að vista stað'
         })
     },
     del () {
+      this.success = false
       const apiUrl = process.env.STUDNINGSBANKINN_API_URL + '/places'
+
       agent
         .del(apiUrl)
         .send(this.place)
         .then(res => {
-          console.log(res.body)
+          if (res.body.id > 0) {
+            this.success = true
+            this.message = 'Aðgerð tókst'
+          }
+        })
+        .catch(e => {
+          this.error = true
+          this.message = 'Ekki tókst að eyða staði'
         })
     }
   }
